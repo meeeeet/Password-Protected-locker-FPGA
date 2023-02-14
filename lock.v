f5d6e44821b64f63638d212b245789f0e178b6e5
Meet Sangani <LegendM3e86338meet@gmail.com>
2023-02-14 10:04:03 +0530

    bdf file

--- lock.v
+++ lock.v
@@ -2,8 +2,11 @@
 /* -------------------------------------------------------------------------- */
 /*                                 Main module                                */
 /* -------------------------------------------------------------------------- */
+
+
+
 module lock(
-    digit_1,digit_2,digit_3,digit_4,
+    digit,
     start,
     reset,
     clk,
@@ -14,18 +17,14 @@ module lock(
     ci
 );
 
-    input digit_1,digit_2,digit_3,digit_4;
+
+    input [3:0]digit;
     input reset,clk,start;
     output out,buzzer;
-	 reg count2;
-	 initial
-	 begin
-		count2=1;
-	 end
 
     wire [3:0]current_pass;
     wire [2:0]wrong_attempt;
-    wire [3:0]pass_serial;
+   // wire [3:0]pass_serial;
 
     //assign current_pass=16'b0001000100011010;
     output [2:0]count; //verification purpose
@@ -33,13 +32,13 @@ module lock(
     output [3:0]ci;
     assign count=wrong_attempt; //verification purpose
     assign cp=current_pass; //verification purpose
-    assign ci=pass_serial;
+    assign ci=digit;
 
-    hextobin h1(digit_1,digit_2,digit_3,digit_4,pass_serial);
+    //hextobin h1(digit_1,digit_2,digit_3,digit_4,pass_serial);
 
-    compare cmp(clk,pass_serial,current_pass,reset,wrong_attempt,out,start);
+    compare cmp(clk,digit,current_pass,wrong_attempt,out);
 
-    update u1(current_pass,pass_serial,clk,reset,start,out);
+    update u1(current_pass,digit,reset,start,out);
 
     buzzer_ctrl buzz(wrong_attempt,buzzer,out);
 
@@ -54,24 +53,17 @@ endmodule
 
 /* --------- module for comparing input password with saved password -------- */
 module compare(
-    clk,pass_in,current_pass,reset,wrong_attempt,out,start
+    clk,pass_in,current_pass,wrong_attempt,out
 );
     input [3:0] pass_in;
     input [3:0] current_pass;
-    input clk,reset,start;
+    input clk;
     output reg out;
     output reg [2:0]wrong_attempt;
 
-    always @(negedge clk or negedge reset or negedge start) begin
+    always @(negedge clk) begin
 
-        if(reset==0 || start==0)begin
-            //out=0;
-            wrong_attempt=3'b000;
-        end
-        // else if(start==0) begin
-        //     wrong_attempt=3'b000;
-        // end
-        else if(pass_in==current_pass) begin
+        if(pass_in==current_pass) begin
             out=1;
             wrong_attempt=3'b000;
         end
@@ -80,41 +72,24 @@ module compare(
             wrong_attempt=wrong_attempt+1;
             out=0;
         end
-
     end
 endmodule
-
-
-/* --- PISO to convert all 4-digit hex input into binary in serial manner --- */
-module hextobin (
-    digit_1,digit_2,digit_3,digit_4,
-    pass_serial
-);
-
-    input digit_1,digit_2,digit_3,digit_4;
-    output [3:0]pass_serial;
-
-    assign pass_serial[3:0]={digit_1,digit_2,digit_3,digit_4};
-
-endmodule
-
 
 /* ------------------ module for store and update password ------------------ */
 module  update(
     current_pass,
     pass_serial,
-    clk,
     reset,
     start,
     out
 );
     input [3:0]pass_serial;
-    input clk,reset,out,start;
+    input reset,out,start;
     output reg [3:0]current_pass;
 
-    always @(negedge clk or negedge reset or negedge start) begin
+    always @(negedge reset or negedge start) begin
         if (reset==0) begin
-			if(out) begin
+			if(out==1) begin
 					current_pass<=pass_serial;
 				end
         end
